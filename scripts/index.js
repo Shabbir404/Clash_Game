6e782fa6e63daa5d327a4e4061610970c2862719
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2024-10-31 00:43:01 +0600

    add some changes in JS

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+function Play() {
+    // getting home and add class 'hidden'
+    const idFinderHome = document.getElementById('home');
+    idFinderHome.classList.add('hidden');
+     // getting play-ground and remove class'hidden'
+    const idFinderPlayGround = document.getElementById('play-ground');
+    idFinderPlayGround.classList.remove('hidden');
+    
+}
